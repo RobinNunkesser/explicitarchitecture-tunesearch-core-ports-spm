89efc05b6e34138d880a9b1df77d580238efaf84
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-04 14:01:55 +0100

    Version 0.0.1

--- Sources/TunesearchCorePorts/CollectionEntity.swift
+++ Sources/TunesearchCorePorts/CollectionEntity.swift
@@ -4,7 +4,7 @@ public class CollectionEntity {
     let name : String
     let tracks : [TrackEntity]
     
-    init(name: String, tracks: [TrackEntity]) {
+    public init(name: String, tracks: [TrackEntity]) {
         self.name = name
         self.tracks = tracks
     }
