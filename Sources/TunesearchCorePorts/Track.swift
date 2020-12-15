232e611c99bcd8a82561bcf2a60dd6024ae41e34
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-15 22:51:30 +0100

    Version 0.0.3

--- Sources/TunesearchCorePorts/Track.swift
+++ Sources/TunesearchCorePorts/Track.swift
@@ -2,9 +2,11 @@ import Foundation
 
 public protocol Track {
     var trackNumber: Int? {get set}
-    var artistName: String {get set}
+    var artistName: String? {get set}
     var trackName: String? {get set}
-    var collectionName: String {get set}
+    var collectionName: String? {get set}
     var discNumber: Int? {get set}
-    var artworkUrl: URL {get set}    
+    var artworkUrl: URL? {get set}    
 }
+
+
